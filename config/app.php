a43ad16c41b04ac49ab3ff09d5cd9deddbf84bfe
Sirwan Veisi <sirwan.dev@gmail.com>
2022-07-15 18:25:03 +0430

    update to latest version of Laravel & Packages

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        Hekmatinasser\Verta\VertaServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,7 +228,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'Verta' => Hekmatinasser\Verta\Verta::class,
 
     ],
 
